0a0d472c9cee8eceef4c3373c08530b40da115c9
bubleeen56 <151603995+bubleeen56@users.noreply.github.com>
2024-01-23 12:07:02 +0900

    registration commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -7,5 +8,10 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == 'admin' && password == '2222'
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password, :password_confirmation, :first_name_kanji, :last_name_kanji, :first_name, :last_name, :birth])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
   end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,33 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_prototype, only: [:show, :edit, :update, :destroy]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = current_user.items.build(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_item
+    @item = item.find(params[:id])
+  end
+
+  def move_to_index
+    unless current_user == @item.user
+     redirect_to action: :index
+    end
   end
 end
